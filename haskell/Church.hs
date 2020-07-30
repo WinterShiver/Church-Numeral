a58f4d1501e5b1b440bd511f0f932d0901ab3374
WinterShiver <916681033@qq.com>
2020-07-30 11:40:23 +0800

    add much on church numeral

--- haskell/Church.hs
+++ haskell/Church.hs
@@ -1,8 +1,13 @@
 -- Church.hs
 -- Church Numeral: https://karczmarczuk.users.greyc.fr/Essays/church.html
 
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE RankNTypes #-}
+-- {-# LANGUAGE NoMonomorphismRestriction #-}
+-- {-# LANGUAGE RankNTypes #-}
+
+{-# LANGUAGE LambdaCase #-}
+
+import Control.Applicative (liftA2)
+import Data.Function ((&))
 
 -- Part 1. Defining church numbers
 
@@ -14,10 +19,11 @@ type Church a = (a -> a) -> a -> a
 zero :: Church a
 zero s z = z  
 -- or: zero = const id
+-- or: zero = flip const
 
 incr :: Church a -> Church a
-incr n s = n s . s  
--- or: incr n s = s . n s
+incr = liftA2 (.) id -- from: incr n s = s . n s
+-- or: incr n s = n s . s  
 
 -- Frequently used church values
 
@@ -49,9 +55,51 @@ num2ch n = case compare n 0 of
 
 -- Part 2. Defining operators on church numbers
 
-add :: Church (Church b) -> Church b -> Church b
-add n m = n incr m
--- or: add n m s = n s . m s
+-- binary oprs: add, mul, pow
 
--- TODO: auto inference of tape var. 
---       eg. in add, etc. 
+add :: Church a -> Church a -> Church a
+add = liftA2 (.) -- from: add n m s z = n s (m s z)
+-- or: add n m = n incr m, but this causes complicated type 
+
+mul :: Church a -> Church a -> Church a
+mul = (.) -- from: mul n m s z = n (m s) z
+-- or: mul n m = n (add m) zero, but this causes complicated type 
+
+pow :: Church a -> Church (a -> a) -> Church a
+pow = (&)
+-- from: pow n m = m n, this has caused complicated type
+-- or: pow n m = m (mul n) one, but this causes complicated type 
+
+-- predecessor (decr) and subtraction 
+
+decr :: Church ((a -> a) -> a) -> Church a
+decr n s z = ($ id) $ n ((&) . ($ s)) (const z) -- from: decr n s z = n (\g h -> h (g s)) (const z) id
+
+
+-- not able to construct sub n m = n decr m
+
+
+
+-- Part 3. Peano numbers (Nats)
+-- Basic oprs, transformation between chs and nats, and so on
+
+data Nat = Zero | Succ Nat deriving (Show)
+
+fold :: (a -> a) -> a -> Nat -> a
+fold s z Zero = z
+fold s z (Succ n) = s (fold s z n)
+
+-- fold s z n = (nat2ch n) s z
+
+padd = fold Succ -- padd n m = fold Succ n m
+pmul = ($ Zero). fold . padd -- pmul n m = fold (padd n) Zero m
+ppow = ($ Succ Zero) . fold . pmul -- ppow n m = fold (pmul n) (Succ Zero) m
+
+nat2ch = fold incr zero -- nat2ch n = fold incr zero n
+ch2nat = ($ Zero) . ($ Succ) -- ch2nat ch = ch Succ Zero
+
+pincr = Succ
+pdecr = \case 
+  Zero -> Zero
+  Succ n -> n
+
